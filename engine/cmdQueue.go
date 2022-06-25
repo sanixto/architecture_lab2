c7c3034b37f3adf53220622ccd8bf9597159bae4
sanixto <sanix.to@gmail.com>
2022-06-25 20:48:56 +0300

    implemented commands queue

---
+++ engine/cmdQueue.go
@@ -0,0 +1,43 @@
+package engine
+
+import "sync"
+
+type cmdQueue struct {
+	mu   sync.Mutex
+	a    []Command
+	wait bool
+
+	notEmpty chan struct{}
+}
+
+func (cq *cmdQueue) push(cmd Command) {
+	cq.mu.Lock()
+	cq.a = append(cq.a, cmd)
+	cq.mu.Unlock()
+	if cq.wait {
+		cq.notEmpty <- struct{}{}
+	}
+}
+
+func (cq *cmdQueue) pull() Command {
+	cq.mu.Lock()
+	defer cq.mu.Unlock()
+
+	if len(cq.a) == 0 {
+		cq.wait = true
+		cq.mu.Unlock()
+		<-cq.notEmpty
+		cq.mu.Lock()
+	}
+
+	res := cq.a[0]
+	cq.a[0] = nil
+	cq.a = cq.a[1:]
+	return res
+}
+
+func (cq *cmdQueue) empty() bool {
+	cq.mu.Lock()
+	defer cq.mu.Unlock()
+	return len(cq.a) == 0
+}
